1d42f10d3d02c26c8e0f5090baa7b5eb4dcd5acb
derdrache <1297886375@qq.com>
2019-12-16 23:46:45 +0800

    集合的练习

--- src/main/java/com/der/codepratise/BasicUtilities/ThrowablesClient.java
+++ src/main/java/com/der/codepratise/BasicUtilities/ThrowablesClient.java
@@ -2,6 +2,9 @@ package com.der.codepratise.BasicUtilities;
 
 import com.google.common.base.Throwables;
 
+import java.io.InputStream;
+import java.net.URL;
+
 /**
  * 简化异常和错误的传播与检查
  * @author K0790016
@@ -10,10 +13,23 @@ public class ThrowablesClient {
 
     public static void main(String[] args) {
 
+//        try {
+//            throw new IOException("sddf");
+//        } catch (Throwable e) {
+//            System.out.println(Throwables.getStackTraceAsString(e));
+//            Throwables.propagateIfPossible(e, RuntimeException.class);
+//            Throwables.getCauseAs()
+//        }
+
+        //针对一些检查异常，在需要的时候将检查异常转化为非检查异常
+
         try {
-            throw new RuntimeException("sddf");
+            URL url = new URL("http://ociweb.com");
+            final InputStream in = url.openStream();
+            // read from the input stream
+            in.close();
         } catch (Exception e) {
-            Throwables.propagateIfPossible(e, RuntimeException.class);
+            Throwables.throwIfUnchecked(e);
         }
     }
 }
