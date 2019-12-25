aa6496e5f2e14b528d651fa803c498540ca9a92c
derdrache <1297886375@qq.com>
2019-12-25 21:22:12 +0800

    缓存操作

--- src/main/java/com/der/codepratise/CachesExplained/CachesExplained.java
+++ src/main/java/com/der/codepratise/CachesExplained/CachesExplained.java
@@ -2,8 +2,12 @@ package com.der.codepratise.CachesExplained;
 
 import com.der.codepratise.entity.MapTestEntity;
 import com.google.common.cache.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 
 import java.time.Duration;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 
 /**
  * @program: guava-code-pratise
@@ -11,15 +15,16 @@ import java.time.Duration;
  * @author: long
  * @create: 2019-12-25 17:27
  */
+@Slf4j
 public class CachesExplained {
 
     public static void main(String[] args) {
 
         //从LoadingCache查询的正规方式是使用get(K)方法。这个方法要么返回已经缓存的值，要么使用CacheLoader向缓存原子地加载新值
-        LoadingCache<String, MapTestEntity> loadingCache = CacheBuilder.newBuilder().maximumSize(2000L).expireAfterAccess(Duration.ofDays(1L)).removalListener(new RemovalListener<String, MapTestEntity>() {
+        LoadingCache<String, MapTestEntity> loadingCache = CacheBuilder.newBuilder().maximumSize(2000L).expireAfterAccess(Duration.ofMillis(300)).removalListener(new RemovalListener<String, MapTestEntity>() {
             @Override
             public void onRemoval(RemovalNotification<String, MapTestEntity> notification) {
-
+                log.info("remove key {} with value {}", notification.getKey(), notification.getValue());
             }
         }).build(new CacheLoader<String, MapTestEntity>() {
             @Override
@@ -28,14 +33,40 @@ public class CachesExplained {
             }
         });
 
-        new ForwardingLoadingCache<String,MapTestEntity>(){
+        ForwardingLoadingCache<String, MapTestEntity> forwardingLoadingCache = new ForwardingLoadingCache<String, MapTestEntity>() {
 
             @Override
             protected LoadingCache<String, MapTestEntity> delegate() {
-                return null;
+                return loadingCache;
             }
+        };
+
+        try {
+            MapTestEntity mapTestEntity = forwardingLoadingCache.get("33");
+            Assert.assertEquals("MapTestEntity(id=null, name=33, sex=null, description=null)", mapTestEntity.toString());
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
 
-        loadingCache.getUnchecked()
+        try {
+            Callable<MapTestEntity> callable = new Callable<MapTestEntity>() {
+                @Override
+                public MapTestEntity call() throws Exception {
+                    return new MapTestEntity(0, "zero");
+                }
+            };
+            MapTestEntity zero = forwardingLoadingCache.get("44", callable);
+            Assert.assertEquals("MapTestEntity(id=0, name=zero, sex=null, description=null)", zero.toString());
+            //cacheloader 中 load只执行了一次，说明第一次获取33后被缓存了起来
+            MapTestEntity mapTestEntity = forwardingLoadingCache.get("33", callable);
+            Assert.assertEquals("MapTestEntity(id=null, name=33, sex=null, description=null)", mapTestEntity.toString());
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        MapTestEntity unchecked = loadingCache.getUnchecked("5");
+        Assert.assertEquals("MapTestEntity(id=null, name=5, sex=null, description=null)", unchecked.toString());
+
     }
 }
